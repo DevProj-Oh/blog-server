84f98427546fc3098d3c1e750d87cccb5becddad
hui-ho <hui-ho@outlook.com>
2020-01-02 18:00:12 +0800

    test: article detail api test

--- tests/Feature/ArticleTest.php
+++ tests/Feature/ArticleTest.php
@@ -19,9 +19,6 @@ class ArticleTest extends TestCase
             'category_id' =>  $category->id,
         ]);
 
-        $tag = factory(Tag::class)->create();
-        $article->tags()->attach($tag->id);
-
         $this->get('/api/articles')
             ->assertOk()
             ->assertJsonStructure(['data' => []])
@@ -30,18 +27,44 @@ class ArticleTest extends TestCase
                     'article_id' => $article->id,
                     'title' => $article->title,
                     'descript' => $article->descript,
-                    'content' => $article->html_content,
                     'category' => [
                         'category_id' => $category->id,
                         'name' => $category->name,
                     ],
-                    'tags' => [
-                        [
-                            'tag_id' => $tag->id,
-                            'name' => $tag->name,
-                        ]
-                    ],
+                    'updated_at' => $article->updated_at->diffForHumans(),
                 ]
+            ]]);
+    }
+
+    public function testGetAnArticleDetail()
+    {
+        $category = factory(Category::class)->create();
+        $article = factory(Article::class)->create([
+            'category_id' =>  $category->id,
+        ]);
+
+        $tag = factory(Tag::class)->create();
+        $article->tags()->attach($tag->id);
+
+        $this->get('/api/articles/' . $article->id)
+            ->assertOk()
+            ->assertJsonStructure(['data' => []])
+            ->assertJsonFragment(['data' => [
+                'article_id' => $article->id,
+                'title' => $article->title,
+                'descript' => $article->descript,
+                'content' => $article->html_content,
+                'category' => [
+                    'category_id' => $category->id,
+                    'name' => $category->name,
+                ],
+                'tags' => [
+                    [
+                        'tag_id' => $tag->id,
+                        'name' => $tag->name,
+                    ]
+                ],
+                'updated_at' => $article->updated_at->diffForHumans(),
             ]]);
     }
 }
